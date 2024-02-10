8b7b691ffb9dacfc26a690c7a90b8f9a33619776
NG <bbcn419a@gmail.com>
2024-02-10 18:27:41 +0300

    first commit

---
+++ laravel_api/routes/api.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ContactController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::get('contacts', [ContactController::class, 'contacts']);
+Route::post('save_contact', [ContactController::class, 'saveContact']);
+Route::post('delete_contact/{id}', [ContactController::class, 'deleteContact']);
+Route::get('get_contact/{id}', [ContactController::class, 'getContact']);
+Route::post('update_contact/{id}', [ContactController::class, 'updateContact']);
